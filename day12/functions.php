16b6c84bc0541271d2168c865faf1c599d6506a8
j_polak <jan.polak@onmyway.cz>
2017-01-24 14:44:48 +0100

    PHP variables & operators

--- day12/functions.php
+++ day12/functions.php
@@ -7,15 +7,36 @@ function get_current_year()
 
 function print_current_year()
 {
+
+
     echo get_current_year();
 }
 
 function render_page($page_file) {
+
+
+
     include 'upper-content.php';
         
     include $page_file;
 
     include 'lower-content.php';
+}
+
+function print_greeting()
+{
+    global $my_greeting;
+
+    echo $my_greeting;
+}
+
+/**
+ * dump & die
+ */
+function dd($variable)
+{
+    var_dump($variable);
+    die();
 }
 
 ?>
--- day12/index.php
+++ day12/index.php
@@ -1,9 +1,50 @@
 <?php
 
-$my_password = 'Slartibartfast';
+define('IN_PRODUCTION', false);
 
-echo 'Hello world!'; 
+require 'functions.php';
 
-phpinfo();
+$my_greeting = "Hello, world!";
+
+//print_greeting();
+
+$first_name = 'Jan';
+$surname = 'Polak';
+$year_of_birth = 1982;
+$height = 186;
+
+
+// 100 lines of code
+$first_name = '';
+
+define('MY_OS', 'Windows 10');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP playground</title>
+</head>
+<body>
+    
+    First name: <?php echo $first_name; ?><br />
+    Surname: <?php echo $surname; ?><br />
+    YOB: <?php echo $year_of_birth; ?><br />
+    Height: <?php echo $height; ?>cm<br />
+
+    My OS is: <?php echo MY_OS; ?>
+
+
+    <?php
+        function celsius_to_fahrenheit($celsius)
+        {
+            $fahrenheit = (9/5) * $celsius + 32;
+            return $fahrenheit;
+        }
+    ?>
+    <br />
+    Today is <?php echo celsius_to_fahrenheit(-6); ?>&deg; F
+
+</body>
+</html>
